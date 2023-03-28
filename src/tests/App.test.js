71981e32430a933d0c587dde60ea3abf0b853aad
Pedro Muller <pedromullergl11@gmail.com>
2023-03-28 15:45:31 -0300

    test: teste de links do app.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('O primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', {
+      name: /home/i,
+    });
+
+    expect(linkHome).toBeInTheDocument();
+  });
+  it('O primeiro link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', {
+      name: /about/i,
+    });
+
+    expect(linkAbout).toBeInTheDocument();
+  });
+  it('O primeiro link deve possuir o texto "Favorite Pokemon"', () => {
+    renderWithRouter(<App />);
+
+    const linkFavorite = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
