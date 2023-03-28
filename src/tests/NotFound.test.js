5e6d6fe662cc7de22c15791f0669520a35ae1a8c
Pedro Muller <pedromullergl11@gmail.com>
2023-03-28 17:01:21 -0300

    test: verificado se a aplicacao da Page Not Found funciona corretamente

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import { act, screen } from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa se a aplicação de "Not Found" funciona corretamente', () => {
+  it('Teste se a página contém um heading com o texto "Page requested not found" e se mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    act(() => history.push('/xablau'));
+
+    const notFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    act(() => history.push('/xablau'));
+
+    const imgNotFound = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    const imgLink = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound).toHaveAttribute('src', imgLink);
+  });
+});
