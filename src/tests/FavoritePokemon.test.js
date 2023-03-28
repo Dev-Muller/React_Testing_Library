40708c617ad4c3776c4ab0146715ee6b6b067c21
Pedro Muller <pedromullergl11@gmail.com>
2023-03-28 16:29:15 -0300

    test: feito teste de pokemons favoritos

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import { screen, container } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se ao favoritar a partir da página de detalhes', () => {
+  it('Teste se é exibida na tela a mensagem "No favorite pokemon found", caso a pessoa não tenha Pokémon favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    }));
+    const noFavorite = screen.getByText(/no favorite pokémon found/i);
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  it('Teste se apenas são exibidos os Pokémon favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(screen.getByRole('link', {
+      name: /more details/i,
+    }));
+
+    userEvent.click(screen.getByRole('checkbox', {
+      name: /pokémon favoritado\?/i,
+    }));
+
+    userEvent.click(screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    }));
+
+    // screen.logTestingPlaygroundURL();
+    const favoritePikachu = screen.getByText(/pikachu/i);
+
+    expect(favoritePikachu).toBeInTheDocument();
+  });
+});
