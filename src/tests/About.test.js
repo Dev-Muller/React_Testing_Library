43ee6d842e1ab6d59991538af984580855c65809
Pedro Muller <pedromullergl11@gmail.com>
2023-03-28 16:03:44 -0300

    test: test do component about feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste se a página contém as informações sobre a Pokédex', () => {
+  it(' contém um heading com o texto "About Pokédex"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(screen.getByRole('link', {
+      name: /about/i,
+    }));
+    const headingAbout = screen.getByRole('heading', {
+      name: /about pokédex/i,
+    });
+
+    expect(headingAbout).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('O primeiro link deve possuir o texto "About"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(screen.getByRole('link', {
+      name: /about/i,
+    }));
+
+    const imgAbout = screen.getByRole('img', {
+      name: /pokédex/i,
+    });
+    const imgLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(imgAbout).toBeInTheDocument();
+    expect(imgAbout).toHaveAttribute('src', imgLink);
+  });
+});
