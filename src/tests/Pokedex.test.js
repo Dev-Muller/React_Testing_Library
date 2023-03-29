84bc375c5e19b0e3181169137eb4d1f740a990c6
Pedro Muller <pedromullergl11@gmail.com>
2023-03-29 18:01:43 -0300

    test: refatorado dois testes da Pokedex e movimentado a ordem de alguns testes

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,59 +4,6 @@ import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 describe('Teste se o component "Pokedex" funciona corretamente', () => {
-  it('Teste se os botões de filtragem por tipo possuem o nome correto', () => {
-    const { history } = renderWithRouter(<App />);
-
-    expect(history.location.pathname).toBe('/');
-
-    const allBtn = screen.getAllByRole('button');
-    expect(allBtn).toBeDefined();
-    // expect(allBtn[0].innerHTML).toBe('All');
-    expect(allBtn[1].innerHTML).toBe('Electric');
-    expect(allBtn[2].innerHTML).toBe('Fire');
-    expect(allBtn[3].innerHTML).toBe('Bug');
-    expect(allBtn[4].innerHTML).toBe('Poison');
-    expect(allBtn[5].innerHTML).toBe('Psychic');
-    expect(allBtn[6].innerHTML).toBe('Normal');
-    expect(allBtn[7].innerHTML).toBe('Dragon');
-  });
-
-  it('Teste se os botões de filtragem por tipo possuem o "data-testid=pokemon-type-button" exceto o botão "All"', () => {
-    const { history } = renderWithRouter(<App />);
-
-    expect(history.location.pathname).toBe('/');
-
-    const allBtn = screen.getAllByRole('button');
-    const dataTeste = 'data-testid';
-    const dataID = 'pokemon-type-button';
-    expect(allBtn).toBeDefined();
-    expect(allBtn[0]).not.toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[1]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[2]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[3]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[4]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[5]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[6]).toHaveAttribute(dataTeste, dataID);
-    expect(allBtn[7]).toHaveAttribute(dataTeste, dataID);
-  });
-
-  it('Teste se é possível clicar no botão de filtragem All', () => {
-    const { history } = renderWithRouter(<App />);
-
-    expect(history.location.pathname).toBe('/');
-
-    userEvent.click(screen.getByRole('button', {
-      name: /fire/i,
-    }));
-
-    userEvent.click(screen.getByRole('button', {
-      name: /all/i,
-    }));
-
-    const Pikachu = screen.getByText(/pikachu/i);
-    expect(Pikachu).toBeInTheDocument();
-  });
-
   it('Teste se a página contém um heading com o texto "Encountered Pokémon"', () => {
     const { history } = renderWithRouter(<App />);
 
@@ -87,7 +34,7 @@ describe('Teste se o component "Pokedex" funciona corretamente', () => {
     expect(charmander).toBeInTheDocument();
   });
 
-  it('Teste se é apenas um Pokémon', () => {
+  it('Teste se é apenas um Pokémon exibido na pokedex', () => {
     const { history } = renderWithRouter(<App />);
 
     expect(history.location.pathname).toBe('/');
@@ -95,5 +42,57 @@ describe('Teste se o component "Pokedex" funciona corretamente', () => {
     const img = screen.getAllByRole('img');
 
     expect(img).toHaveLength(1);
+  });
+
+  it('Teste se os botões de filtragem por tipo possuem o nome correto', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    const allBtn = screen.getAllByRole('button');
+    expect(allBtn).toBeDefined();
+    // expect(allBtn[0].innerHTML).toBe('All');
+    expect(allBtn[1].innerHTML).toBe('Electric');
+    expect(allBtn[2].innerHTML).toBe('Fire');
+    expect(allBtn[3].innerHTML).toBe('Bug');
+    expect(allBtn[4].innerHTML).toBe('Poison');
+    expect(allBtn[5].innerHTML).toBe('Psychic');
+    expect(allBtn[6].innerHTML).toBe('Normal');
+    expect(allBtn[7].innerHTML).toBe('Dragon');
+  });
+
+  it('Teste se os botões de filtragem por tipo possuem o "data-testid=pokemon-type-button" exceto o botão "All"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    const allBtn = screen.getAllByTestId('pokemon-type-button');
+    const dataTeste = 'data-testid';
+    const dataID = 'pokemon-type-button';
+    expect(allBtn).toBeDefined();
+    expect(allBtn[0]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[1]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[2]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[3]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[4]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[5]).toHaveAttribute(dataTeste, dataID);
+    expect(allBtn[6]).toHaveAttribute(dataTeste, dataID);
+  });
+
+  it('Teste se é possível clicar no botão de filtragem All', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(screen.getByRole('button', {
+      name: /fire/i,
+    }));
+
+    userEvent.click(screen.getByRole('button', {
+      name: /all/i,
+    }));
+
+    const Pikachu = screen.getByText(/pikachu/i);
+    expect(Pikachu).toBeInTheDocument();
   });
 });
